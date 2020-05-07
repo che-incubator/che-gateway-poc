b0647e9eaf83bbffcbe693b831af8d04f939d882
Michal Vala <mvala@redhat.com>
2020-05-07 16:35:56 +0200

    ignore workspace create in report, request threads variable, readme up

--- perftest.sh
+++ perftest.sh
@@ -6,5 +6,6 @@ JMETER_BIN=jmeter-cli
 REPORT_DIR=perfReport_$( date +%s )
 
 mkdir ${REPORT_DIR}
+cp perftest.jmx ${REPORT_DIR}
 
-${JMETER_BIN} -n -t perftest.jmx -l ${REPORT_DIR}/perftest.log -e  -o ${REPORT_DIR}/perftest_report
+${JMETER_BIN} -n -t ${REPORT_DIR}/perftest.jmx -l ${REPORT_DIR}/perftest.log -e  -o ${REPORT_DIR}/dashboard
