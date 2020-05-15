31abd16fd7aed2c83681ebf516c0539c88f3d95a
Michal Vala <mvala@redhat.com>
2020-05-15 11:04:25 +0200

    [test] - jmeter log to report dir

--- test/02_test.sh
+++ test/02_test.sh
@@ -8,4 +8,4 @@ set -x
 JMETER_BIN=jmeter-cli
 
 prepareTestStructure
-${JMETER_BIN} -n -t ${JMETER_TEST_FILE} -l ${REPORT_DIR}/test.log -e  -o ${REPORT_DIR}/dashboard
+${JMETER_BIN} -n -t ${JMETER_TEST_FILE} -l ${REPORT_DIR}/test.log -j ${REPORT_DIR}/jmeter.log -e  -o ${REPORT_DIR}/dashboard
