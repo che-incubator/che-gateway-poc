102251128c66fa19460166afd6aad1a811ee0317
Lukas Krejci <lkrejci@redhat.com>
2020-05-15 17:18:50 +0200

    Fix bugs in the gateway.sh and try to support cookies and websockets in nginx config.

--- nginx-custom-image/functions/gateway.sh
+++ nginx-custom-image/functions/gateway.sh
@@ -1,23 +1,27 @@
 #!/bin/sh
 
 function FullGatewayReconfig() {
-    mkdir -p CONFIGMAPS_DIR
-    rm -Rf CONFIGMAPS_DIR/*
+    mkdir -p ${CONFIGMAPS_DIR}
+    rm -Rf ${CONFIGMAPS_DIR}/*
 
     NAME=0
-    while IFS=, read -r PATH SERVICE; do
+    while IFS=, read -r WS_PATH SERVICE; do
         CONFIG_MAP="
           apiVersion: v1
           kind: ConfigMap
-          metatada:
+          metadata:
             name: ws-$NAME
             labels:
                 che-config-role: gateway
           data:
             ws-$NAME: |
-              location ${PATH} {
+              location /${WS_PATH} {
                 proxy_pass http://${SERVICE};
-            }
+                proxy_cookie_path \$uri /${WS_PATH}\$uri;
+                proxy_http_version 1.1;
+                proxy_set_header Upgrade \$http_upgrade;
+                proxy_set_header Connection \$connection_upgrade;
+              }
         "
         echo "${CONFIG_MAP}" | oc apply -n ${POC_NAMESPACE} -f -
         NAME=$(($NAME + 1))
