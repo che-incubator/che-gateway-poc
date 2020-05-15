bebd138a17db3f97dedc22a2c57f4b06b3c3f2ad
Michal Vala <mvala@redhat.com>
2020-05-15 15:19:30 +0200

    [test] - don't add / to path for che base url

--- test/functions/prepare.sh
+++ test/functions/prepare.sh
@@ -13,7 +13,7 @@ function prepareBaseInfra() {
   sed "s/{{HOST}}/${HOST}/g" ${YAMLS_DIR}/openshift.yaml_template | oc apply -n ${POC_NAMESPACE} -f -
 
   # add Che to tested URLs
-  echo "che,${HOST},/" >> ${URLS_CSV}
+  echo "che,${HOST}," >> ${URLS_CSV}
 
   echo "http://${HOST}"
 }
