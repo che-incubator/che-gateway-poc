61a4e8a7fc3dbfd7838538419a55be7d769721f1
Michal Vala <mvala@redhat.com>
2020-05-15 14:38:46 +0200

    [test] - run test in dockerimage

--- test/02_run.sh
+++ test/02_run.sh
@@ -2,10 +2,14 @@
 
 set -e
 
-set -x
 . "$( dirname "${0}" )/env.sh"
 
-JMETER_BIN=jmeter-cli
-
 prepareTestStructure
-${JMETER_BIN} -n -t ${JMETER_TEST_FILE} -l ${REPORT_DIR}/test.log -j ${REPORT_DIR}/jmeter.log -e  -o ${REPORT_DIR}/dashboard
+
+HOST_IP="$( getent hosts ${HOST} | awk '{ print $1 }' )"
+docker run --rm \
+-v ${REPORT_DIR}:${REPORT_DIR}:Z \
+-v ${WORKDIR}:${WORKDIR}:Z \
+--add-host ${HOST}:${HOST_IP} \
+justb4/jmeter:5.1.1 \
+-n -t ${JMETER_TEST_FILE} -l ${REPORT_DIR}/test.log -j ${REPORT_DIR}/jmeter.log -e -o ${REPORT_DIR}/dashboard
