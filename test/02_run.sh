17c15cea39a556a264f174f1a76f86eaef03ff93
Michal Vala <mvala@redhat.com>
2020-05-19 19:17:43 +0200

    [test] - tc1

--- test/02_run.sh
+++ test/02_run.sh
@@ -13,23 +13,22 @@ function run() {
 
   docker run --rm \
   -v ${REPORT_DIR}:${REPORT_DIR}:Z \
-  -v ${WORKDIR}:${WORKDIR}:Z \
+  -v ${BASE_DIR}:${BASE_DIR}:Z \
   --add-host ${HOST}:${HOST_IP} \
   justb4/jmeter:5.1.1 \
   -n -Jjmeter.reportgenerator.overall_granularity=1000 -e \
   -t ${JMETER_TEST_FILE} -l ${REPORT_DIR}/test.log -j ${REPORT_DIR}/jmeter.log -o ${REPORT_DIR}/dashboard "${@}"
 
   cat ${REPORT_DIR}/dashboard/statistics.json
-  echo "${GATEWAY},${TESTCASE},$( IFS=","; echo "${LINE[*]}" ),$( cat ${REPORT_DIR}/dashboard/statistics.json | jq -r '.Total | [.sampleCount, .errorCount, .errorPct, .meanResTime, .minResTime, .maxResTime, .pct1ResTime, .pct2ResTime, .pct3ResTime, .throughput, .receivedKBytesPerSec, .sentKBytesPerSec] | join(",")' )" >> ${TEST_STATS_FILE}
+  if [ -f ${TEST_PARAMS_FILE} ]; then
+    echo "${GATEWAY},${TESTCASE},$( IFS=","; echo "${LINE[*]}" ),$( cat ${REPORT_DIR}/dashboard/statistics.json | jq -r '.Total | [.sampleCount, .errorCount, .errorPct, .meanResTime, .minResTime, .maxResTime, .pct1ResTime, .pct2ResTime, .pct3ResTime, .throughput, .receivedKBytesPerSec, .sentKBytesPerSec] | join(",")' )" >> ${TEST_STATS_FILE}
+  fi
 }
 
-
-if [ ! -f ${TEST_STATS_FILE} ]; then
-  echo "gateway,testcase,$( head -n 1 ${TEST_PARAMS_FILE} ),sampleCount,errorCount,errorPct,meanResTime,minResTime,maxResTime,pct90ResTime,pct95ResTime,pct99ResTime,throughput,receivedKBytesPerSec,sentKBytesPerSec" > ${TEST_STATS_FILE}
-fi
 # if there is params.csv in the testcase, read this file and run in the loop
 # otherwise run once with default parameters from 'test.jmx' file
 if [ -f ${TEST_PARAMS_FILE} ]; then
+  echo "gateway,testcase,$( head -n 1 ${TEST_PARAMS_FILE} ),sampleCount,errorCount,errorPct,meanResTime,minResTime,maxResTime,pct90ResTime,pct95ResTime,pct99ResTime,throughput,receivedKBytesPerSec,sentKBytesPerSec" > ${TEST_STATS_FILE}
   # read 1st line of csv to have parameter names
   IFS=',' read -r -a PARAMS <<< "$( head -n 1 ${TEST_PARAMS_FILE} )"
 
