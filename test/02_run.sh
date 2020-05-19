c3b18549fe6b56935d1864daa03bf01dd1234d5a
Michal Vala <mvala@redhat.com>
2020-05-19 12:26:50 +0200

    [test] - generic reading parameters from csv

--- test/02_run.sh
+++ test/02_run.sh
@@ -20,24 +20,35 @@ function run() {
   -t ${JMETER_TEST_FILE} -l ${REPORT_DIR}/test.log -j ${REPORT_DIR}/jmeter.log -o ${REPORT_DIR}/dashboard "${@}"
 
   cat ${REPORT_DIR}/dashboard/statistics.json
-  echo "${GATEWAY},${TESTCASE},${DURATION},${LOOP_DELAY},${REQUEST_THREADS},${WORKSPACES_CREATE},${WORKSPACE_DELAY},${WORKSPACE_INITIAL_DELAY},$( cat ${REPORT_DIR}/dashboard/statistics.json | jq -r '.Total | [.sampleCount, .errorCount, .errorPct, .meanResTime, .minResTime, .maxResTime, .pct1ResTime, .pct2ResTime, .pct3ResTime, .throughput, .receivedKBytesPerSec, .sentKBytesPerSec] | join(",")' )" >> ${TEST_STATS_FILE}
+  echo "${GATEWAY},${TESTCASE},$( IFS=","; echo "${LINE[*]}" ),$( cat ${REPORT_DIR}/dashboard/statistics.json | jq -r '.Total | [.sampleCount, .errorCount, .errorPct, .meanResTime, .minResTime, .maxResTime, .pct1ResTime, .pct2ResTime, .pct3ResTime, .throughput, .receivedKBytesPerSec, .sentKBytesPerSec] | join(",")' )" >> ${TEST_STATS_FILE}
 }
 
 
 if [ ! -f ${TEST_STATS_FILE} ]; then
-  echo "gateway,testcase,duration,loop_delay,request_threads,workspaces_create,workspace_delay,workspace_initial_delay,sampleCount,errorCount,errorPct,meanResTime,minResTime,maxResTime,pct90ResTime,pct95ResTime,pct99ResTime,throughput,receivedKBytesPerSec,sentKBytesPerSec" > ${TEST_STATS_FILE}
+  echo "gateway,testcase,$( head -n 1 ${TEST_PARAMS_FILE} ),sampleCount,errorCount,errorPct,meanResTime,minResTime,maxResTime,pct90ResTime,pct95ResTime,pct99ResTime,throughput,receivedKBytesPerSec,sentKBytesPerSec" > ${TEST_STATS_FILE}
 fi
 # if there is params.csv in the testcase, read this file and run in the loop
 # otherwise run once with default parameters from 'test.jmx' file
 if [ -f ${TEST_PARAMS_FILE} ]; then
-  sed 1d ${TEST_PARAMS_FILE} | while IFS=, read -r DURATION LOOP_DELAY REQUEST_THREADS WORKSPACES_CREATE WORKSPACE_DELAY WORKSPACE_INITIAL_DELAY; do
-    run "-Jduration=${DURATION}" \
-    "-Jloop_delay=${LOOP_DELAY}" \
-    "-Jrequest_threads=${REQUEST_THREADS}" \
-    "-Jworkspaces_create=${WORKSPACES_CREATE}" \
-    "-Jworkspace_delay=${WORKSPACE_DELAY}" \
-    "-Jworkspace_initial_delay=${WORKSPACE_INITIAL_DELAY}" \
-    "-Jjmeter.reportgenerator.report_title=loop_delay=${LOOP_DELAY};request_threads=${REQUEST_THREADS};workspaces_create=${WORKSPACES_CREATE};workspace_delay=${WORKSPACE_DELAY};workspace_initial_delay=${WORKSPACE_INITIAL_DELAY}"
+  # read 1st line of csv to have parameter names
+  IFS=',' read -r -a PARAMS <<< "$( head -n 1 ${TEST_PARAMS_FILE} )"
+
+  # read rest of file line by line
+  sed 1d ${TEST_PARAMS_FILE} | while read -r LINE; do
+    JMETER_ARGS=""
+    JMETER_REPORT_TITLE=""
+    # split line by ','
+    IFS=',' read -r -a LINE <<< "${LINE}"
+    # and join all params with names to jmeter parameter and report title
+    for i in "${!LINE[@]}"; do
+      JMETER_ARGS="${JMETER_ARGS} -J${PARAMS[i]}=${LINE[i]}"
+      JMETER_REPORT_TITLE="${JMETER_REPORT_TITLE};${PARAMS[i]}=${LINE[i]}"
+    done
+    # join report title to rest of the args
+    JMETER_ARGS="${JMETER_ARGS} -Jjmeter.reportgenerator.report_title=${JMETER_REPORT_TITLE}"
+
+    # and run
+    run "${JMETER_ARGS}"
   done
 else
   run
