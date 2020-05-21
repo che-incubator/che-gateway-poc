b0ecc1ef01c14e0539e25184b2b57e103132210e
Lukas Krejci <lkrejci@redhat.com>
2020-05-21 16:47:53 +0200

    nginx is VERY picky about the locations... need to specialize for the same thing with and without trailing slash.

--- nginx-custom-image/functions/gateway.sh
+++ nginx-custom-image/functions/gateway.sh
@@ -12,6 +12,13 @@ function FullGatewayReconfig() {
                 che-config-role: gateway
           data:
             ws-$NAME: |
+              location /${WS_PATH}/ {
+                proxy_pass http://${SERVICE}/;
+                proxy_cookie_path \$uri /${WS_PATH}\$uri;
+                proxy_http_version 1.1;
+                proxy_set_header Upgrade \$http_upgrade;
+                proxy_set_header Connection \$connection_upgrade;
+              }
               location /${WS_PATH} {
                 proxy_pass http://${SERVICE}/;
                 proxy_cookie_path \$uri /${WS_PATH}\$uri;
