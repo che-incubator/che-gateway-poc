1e9e66881b8df9f723765a5a3a858e13291a9dd1
Lukas Krejci <lkrejci@redhat.com>
2020-05-21 18:51:07 +0200

    Implement AddSingleRoute on nginx-custom-image.

--- nginx-custom-image/env.sh
+++ nginx-custom-image/env.sh
@@ -1,3 +1,15 @@
 #!/bin/sh
 
-# nothing to do here
+NGINX_NWS_FILE="${WORKDIR}/nof_ws.txt"
+
+function NginxGetNumberOfWorkspaces() {
+    if [ ! -e ${NGINX_NWS_FILE} ]; then 
+        echo 0
+    else
+        cat ${NGINX_NWS_FILE}
+    fi
+}
+
+function NginxSetNumberOfWorkspaces() {
+    echo $1 > ${NGINX_NWS_FILE}
+}
--- nginx-custom-image/functions/gateway.sh
+++ nginx-custom-image/functions/gateway.sh
@@ -1,33 +1,45 @@
 #!/bin/sh
 
 function FullGatewayReconfig() {
-    NAME=0
     while IFS=, read -r WS_PATH SERVICE; do
-        CONFIG_MAP="
-          apiVersion: v1
-          kind: ConfigMap
-          metadata:
-            name: ws-$NAME
-            labels:
-                che-config-role: gateway
-          data:
-            ws-$NAME: |
-              location /${WS_PATH}/ {
-                proxy_pass http://${SERVICE}/;
-                proxy_cookie_path \$uri /${WS_PATH}\$uri;
-                proxy_http_version 1.1;
-                proxy_set_header Upgrade \$http_upgrade;
-                proxy_set_header Connection \$connection_upgrade;
-              }
-              location /${WS_PATH} {
-                proxy_pass http://${SERVICE}/;
-                proxy_cookie_path \$uri /${WS_PATH}\$uri;
-                proxy_http_version 1.1;
-                proxy_set_header Upgrade \$http_upgrade;
-                proxy_set_header Connection \$connection_upgrade;
-              }
-        "
-        echo "${CONFIG_MAP}" | oc apply -n ${POC_NAMESPACE} -f -
-        NAME=$(($NAME + 1))
+      AddSingleRoute ${WS_PATH} ${SERVICE}
     done < ${WORKSPACES_DB}    
+}
+
+function AddSingleRoute() {
+  WS_PATH=${1}
+  SERVICE=${2}
+
+  NUMBER_OF_WORKSPACES=`NginxGetNumberOfWorkspaces`
+  N=$(($NUMBER_OF_WORKSPACES + 1))
+  NAME=$(printf "%08d" $N)
+
+  CONFIG_MAP="
+    apiVersion: v1
+    kind: ConfigMap
+    metadata:
+      name: ws-$NAME
+      labels:
+          che-config-role: gateway
+    data:
+      ws-$NAME: |
+        location /${WS_PATH}/ {
+          proxy_pass http://${SERVICE}/;
+          proxy_cookie_path \$uri /${WS_PATH}\$uri;
+          proxy_http_version 1.1;
+          proxy_set_header Upgrade \$http_upgrade;
+          proxy_set_header Connection \$connection_upgrade;
+        }
+        location /${WS_PATH} {
+          proxy_pass http://${SERVICE}/;
+          proxy_cookie_path \$uri /${WS_PATH}\$uri;
+          proxy_http_version 1.1;
+          proxy_set_header Upgrade \$http_upgrade;
+          proxy_set_header Connection \$connection_upgrade;
+        }
+  "
+  echo "${CONFIG_MAP}" | oc apply -n ${POC_NAMESPACE} -f -
+  
+  NUMBER_OF_WORKSPACES=$(($NUMBER_OF_WORKSPACES + 1))
+  NginxSetNumberOfWorkspaces ${NUMBER_OF_WORKSPACES}
 }
