a9190ec9fc0fd81f9569c10c79edf2cac9d4e9e5
Lukas Krejci <lkrejci@redhat.com>
2020-05-28 17:25:50 +0200

    Convert tc_2 to websockets.

---
+++ test/testcases/tc_2-ws/actions.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+set -e
+
+sleep 30
+sh -x 66_addPreparedWorkspace.sh
---
+++ test/testcases/tc_2-ws/prepare.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+function prepareTestcase() {
+  # prepare 26 workspaces
+  for I in {001..030}; do
+    local WS="ws-${I}"
+    prepareNewWorkspace ${WS}
+  done
+
+  # actualy create infrastructure for all
+  createPreparedWorkspacesInfra
+
+  # test only with 25 for now. add last one in actions.sh
+  writeTestAndFlushPreparedWorkspaces 25
+}
